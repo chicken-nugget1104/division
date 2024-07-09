509618eaf8f431d68c6aa9e59912a3eda539e59b
jmxamongusmodder <liamkenny826@gmail.com>
2024-07-09 18:26:20 -0400

    shueisha

--- netlify/functions/create-account.js
+++ netlify/functions/create-account.js
@@ -1,5 +1,4 @@
 const { createClient } = require('@supabase/supabase-js');
-const crypto = require('crypto');
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
@@ -9,12 +8,9 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 exports.handler = async function(event, context) {
   const { username, password, email } = JSON.parse(event.body);
 
-  // Hash the password
-  const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-
   const { data, error } = await supabase
     .from('users')
-    .insert([{ username, password: hashedPassword, email, moons: 25 }]);
+    .insert([{ username, password: password, email, moons: 25 }]);
 
   if (error) {
     return {
